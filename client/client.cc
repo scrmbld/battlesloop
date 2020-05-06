ae17dc03eece8fdbec901a56b6a93f355ce63f67
scrmbld <liamallen792@gmail.com>
2020-05-06 12:53:51 -0700

    set up ncurses

--- client/client.cc
+++ client/client.cc
@@ -3,12 +3,20 @@
 #include <zhelpers.hpp>
 #include <string>
 #include <vector>
+#include <ncurses.h>
+#include <unistd.h>
 using namespace std;
 using namespace zmq;
 
 int main() {
-	cout << "Hello World!\n";
-
+	system("clear");
+	initscr();
+	noecho();
+	clear();
+	move(0,0);
+	printw("Hello World!\n");
+	refresh();
+	
 	context_t context(1);
 	socket_t socket(context, ZMQ_REQ);
 	socket.setsockopt(ZMQ_IPV6, 1);
@@ -16,10 +24,15 @@ int main() {
 	int port = 1533;
 	
 	socket.connect("tcp://" + hostname + ":" + to_string(port));
-	s_send(socket, string("h"));
+	s_send(socket, "h");
 
 	string response = s_recv(socket);
-	cout << "received response from server: " << response << endl;
+	move(40, 0);
+	printw("recieved %s", response.c_str());
 
+	getch();
+	clear();
+	endwin();
+	
 	return 0;
 }
