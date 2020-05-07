ddb7dfaea95961f4f70ab783c85f6922f6d4de80
scrmbld <liamallen792@gmail.com>
2020-05-07 08:23:40 -0700

    added early board draw

--- client/client.cc
+++ client/client.cc
@@ -5,10 +5,21 @@
 #include <vector>
 #include <ncurses.h>
 #include <unistd.h>
+#include "draw_board.h"
 using namespace std;
 using namespace zmq;
 
 int main() {
+	vector<vector<char>> board;
+	for (int i = 0; i < 10; i++) {
+		vector<char> vec;
+		for (int j = 0; j < 10; j++) {
+			vec.push_back('~');
+		}
+
+		board.push_back(vec);
+	}
+	
 	system("clear");
 	initscr();
 	noecho();
@@ -16,6 +27,13 @@ int main() {
 	move(0,0);
 	printw("Hello World!\n");
 	refresh();
+	WINDOW* win = nullptr;
+	init_board_window(1, 1, win);
+	wrefresh(win);
+	getch();
+	draw_board(board, win);
+	wrefresh(win);
+
 	
 	context_t context(1);
 	socket_t socket(context, ZMQ_REQ);
