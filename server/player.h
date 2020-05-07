aafcbaab6fb580d0a66976701f630c433b605fa2
scrmbld <liamallen792@gmail.com>
2020-05-07 11:01:39 -0700

    prepared player list

---
+++ server/player.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+using std::string;
+
+class Player {
+	private:
+		const size_t uid;
+		Player *opponent;
+		//TODO: add a pointer to the player's board
+	public:
+		const string uname;
+		Player() = delete;
+		Player(string name, size_t id) : uid(id), uname(name) {}
+		
+};
--- server/server.cc
+++ server/server.cc
@@ -1,13 +1,15 @@
-
 #include <iostream>
 #include <zmq.hpp>
 #include <zhelpers.hpp>
 #include <string>
 #include <vector>
+#include <sstream>
+#include "player.h"
 using namespace std;
 using namespace zmq;
 
 int main() {
+	//set up socket
 	cout << "starting server...\n";
 	context_t context(1);
 	socket_t socket(context, ZMQ_REP);
@@ -16,10 +18,15 @@ int main() {
 	
 	socket.bind("tcp://*:" + to_string(port));//listen on port
 
+	//prepare for players
+	vector<Player> players;
+
+	//wait for players to connect
 	while (true) {
 		string read = s_recv(socket);
 		cout << "SERVER: " << "recieved " << read << endl;
-
+		players.push_back(Player(read, players.size()));
+		
 		s_send(socket, string("w"));
 	}
 }
