96c61c3f81eaf1934b0b639b42b50a84d2c2a3a8
scrmbld <liamallen792@gmail.com>
2020-05-08 11:09:46 -0700

    re-added ncurses interface

--- client/client.cc
+++ client/client.cc
@@ -16,14 +16,15 @@ string uname;
 void die(int s = 0) {
 	//TODO: update this when we add back ncurses
 	s = s;
-
+	getch();
+	clear();
+	endwin();
+	
 	cout << "logging out" << endl;
 	if (server && uname != "") {
 		s_send(*server, uname + "\vLOGOUT");
-		cout << "e" << endl;
 		usleep(250'000);
 	}
-	cout << "ee" << endl;
 	exit(EXIT_SUCCESS);
 }
 
@@ -33,27 +34,6 @@ int main() {
 	cin >> uname;
 	cout << "connecting to server..." << flush;
 	signal(SIGINT, die); //disconnect nicely
-	/*
-	   vector<vector<char>> board;
-	   for (int i = 0; i < 10; i++) {
-	   vector<char> vec;
-	   for (int j = 0; j < 10; j++) {
-	   vec.push_back('~');
-	   }
-
-	   board.push_back(vec);
-	   }
-	   */
-	//start ncurses
-
-	/*
-	   WINDOW* win = nullptr;
-	   init_board_window(1, 1, win);
-	   wrefresh(win);
-	   getch();
-	   draw_board(board, win);
-	   wrefresh(win);
-	   */
 
 	//make a socket
 	context_t context(1);
@@ -70,34 +50,43 @@ int main() {
 	string response = s_recv(socket);
 	cout << "found server" << endl;
 	server = &socket;
-	/*
-	   system("clear");
-	   initscr();
-	   noecho();
-	   clear();
-	   move(0,0);
-	   printw("Battlesloop\n");
-	   refresh();
-	   */
+	   
+	//start ncurses
+	int line = 0;
+	system("clear");
+	initscr();
+	noecho();
+	clear();
+	move(0,line);
+	printw("Battlesloop\n");
+	line+= 3;
+	refresh();
 	const int MAXFPS = 60;//cap the frame rate
-	if (response != "ADDED") {
-		cout << "Something went wrong: unable to log in" << endl;
+
+
+	if (response == "ADDED") {
+		mvprintw(line, 0, "Connected successfully");
+		line++;
+	} else {
+		mvprintw(line, 0, "Something went wrong: unable to log in");
+		line++;
 	}
+	refresh();
 	while (true) {
 		s_send(socket, uname + "\v" + "\a");
 		response.clear();
 		response = s_recv(socket);
 		usleep(1'000'000 / MAXFPS);
 		if (response != "\a") {
-			cout << "something went wrong" << endl;
+			mvprintw(line, 0, "something went wrong");
+			line++;
 			break;
 		}
 	}
-	/*
-	   getch();
-	   clear();
-	   endwin();
-	   */	
+	refresh();
+	getch();
+	clear();
+	endwin();
 	cout << response << endl;
 	return 0;
 
