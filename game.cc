4f0cfb7369e2b750be64e2af2d2d49ebc9fe6f9a
scrmbld <liamallen792@gmail.com>
2021-01-08 11:09:18 -0800

    updated gitignore

--- game.cc
+++ game.cc
@@ -10,26 +10,13 @@ int main() {
 	noecho();
 
 	//create boards
-	std::string test_data = "1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111";
 	const int p1_y = 10, p1_x = 5, p2_y = 10, p2_x = 40;
-	Board p1(p1_y, p1_x, "You", test_data);
+	Board p1(p1_y, p1_x, "You");
 	Board p2(p2_y, p2_x, "Enemy");
 	
 	p1.drawBoard();
 	p2.drawBoard();
-	
 	refresh();
-	getch();
-	std::string data = p1.encodeToString();
-	mvprintw(1, 1, "%c", data.at(0));
-	refresh();
-	getch();
-	p2.updateFromString(data);
-	
-	p1.drawBoard();
-	p2.drawBoard();
-	refresh();
-	getch();
 
 	//place ships
 	for (int i = 0; i < 5; i++) {
@@ -45,25 +32,18 @@ int main() {
 	}
 
 	//place ships for p2 (temporary, will not be used for online)
-	for (int i = 0; i < 5; i++) {
-		//take input
-		std::pair<int, int> selected = p2.selectTile();
-		p2.changeTile(selected, 1);
-
-		//update screen
-		p1.drawBoard();
-		p2.drawBoard();
-		refresh();
-
-	}
-	
+	std::string placed = p1.encodeToString();
+	p2.updateFromString(placed);
+	p1.drawBoard();
+	p2.drawBoard();
+	refresh();
 
 	//main game loop
 	while (p1.isAlive() && p2.isAlive()) {
 		//p1's turn
 		std::pair<int, int> selected = p2.selectTile();
 		p2.attackTile(selected);
-		
+
 		//update screen
 		p1.drawBoard();
 		p2.drawBoard();
@@ -76,7 +56,7 @@ int main() {
 	clear;
 	endwin();
 	system("clear");
-	
+
 	std::cout << p1.isAlive() << ", " << p2.isAlive() << std::endl;
 	return 0;
 }
